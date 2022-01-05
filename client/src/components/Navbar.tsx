081b405934a3c4e3d303547add2493754224ff17
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2022-01-05 18:14:57 -0500

    add comments, constant

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -3,6 +3,8 @@ import { AiOutlineClose } from 'react-icons/ai'
 import logo from '../../images/logo.png'
 import { useState } from 'react'
 
+const MENU_ITEMS = ['Market', 'Exchange', 'Tutorials', 'Wallets']
+
 interface NavbarItemProps {
   title: string
   classProps?: string
@@ -17,12 +19,14 @@ export const Navbar = () => {
 
   return (
     <nav className='w-full flex md:justify-center justify-between items-center p-4'>
+      {/* LOGO */}
       <div className='md:flex-[0.5] flex-initial justify-center items-center'>
         <img src={logo} alt='logo' className='w-32 cursor-pointer' />
       </div>
 
+      {/* DESKTOP MENU */}
       <ul className='text-white md:flex hidden list-none flex-row justify-between items-center flex-initial'>
-        {['Market', 'Exchange', 'Tutorials', 'Wallets'].map((item, index) => (
+        {MENU_ITEMS.map((item, index) => (
           <NavbarItem key={item + index} title={item} />
         ))}
         <li className='bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]'>
@@ -30,6 +34,7 @@ export const Navbar = () => {
         </li>
       </ul>
 
+      {/* MOBILE MENU */}
       <div className='flex relative'>
         {toggleMenu ? (
           <AiOutlineClose
@@ -48,15 +53,13 @@ export const Navbar = () => {
           <ul className='z-10 fixed top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in'>
             <li className='text-xl w-full my-2'>
               <AiOutlineClose onClick={() => setToggleMenu(false)} />
-              {['Market', 'Exchange', 'Tutorials', 'Wallets'].map(
-                (item, index) => (
-                  <NavbarItem
-                    key={item + index}
-                    title={item}
-                    classProps='my-2 text-lg'
-                  />
-                )
-              )}
+              {MENU_ITEMS.map((item, index) => (
+                <NavbarItem
+                  key={item + index}
+                  title={item}
+                  classProps='my-2 text-lg'
+                />
+              ))}
             </li>
           </ul>
         )}
