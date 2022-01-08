a0a8e08d98a86ce90c6b9b691a3f92ae4320d784
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2022-01-08 15:06:52 -0500

    start form

--- client/src/components/Welcome.tsx
+++ client/src/components/Welcome.tsx
@@ -7,8 +7,36 @@ import { Loader } from './'
 const commonStyles =
   'min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white'
 
+interface InputProps {
+  placeholder: string
+  name: string
+  type: string
+  value: string
+  handleChange: React.ChangeEventHandler<HTMLInputElement>
+}
+
+const Input = ({
+  placeholder,
+  name,
+  type,
+  value,
+  handleChange,
+}: InputProps) => (
+  <input
+    placeholder={placeholder}
+    name={name}
+    step='0.001'
+    value={value}
+    type={type}
+    onChange={(e) => handleChange(e)}
+    className='my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism'
+  ></input>
+)
+
 export const Welcome = () => {
   const connectWallet = () => {}
+
+  const handleSubmit = () => {}
 
   return (
     <div className='flex w-full justify-center items-center'>
@@ -57,7 +85,47 @@ export const Welcome = () => {
               </div>
             </div>
 
-            {/* <div className=''></div> */}
+            {/* FORM */}
+            <div className=' p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism'>
+              <Input
+                placeholder='Address To'
+                name='addressTo'
+                type='text'
+                handleChange={() => {}}
+              />
+              <Input
+                placeholder='Amount (ETH)'
+                name='amount'
+                type='number'
+                handleChange={() => {}}
+              />
+              <Input
+                placeholder='Keyword (Gif)'
+                name='keyword'
+                type='text'
+                handleChange={() => {}}
+              />
+              <Input
+                placeholder='Enter Message'
+                name='message'
+                type='text'
+                handleChange={() => {}}
+              />
+
+              <div className='h-[1px] w-full bg-gray-400 my-2' />
+
+              {false ? (
+                <Loader />
+              ) : (
+                <button
+                  type='button'
+                  onClick={handleSubmit}
+                  className='text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] rounded-full cursor-pointer'
+                >
+                  Send Now
+                </button>
+              )}
+            </div>
           </div>
         </div>
       </div>
