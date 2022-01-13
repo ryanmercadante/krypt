c792a519d77d03cceb69e7d98c3ddc1a32673c50
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2022-01-12 23:26:23 -0500

    more work on transaction card

--- client/src/components/Transactions.tsx
+++ client/src/components/Transactions.tsx
@@ -1,5 +1,72 @@
 import { useTransactions } from '../context/TransactionContext'
 import dummyData from '../utils/dummyData'
+import { shortenAddress } from '../utils/helpers'
+
+interface TransactionCardProps {
+  id: number
+  url: string
+  message: string
+  timestamp: string
+  addressFrom: string
+  amount: string
+  addressTo: string
+}
+
+const TransactionCard = ({
+  id,
+  url,
+  message,
+  timestamp,
+  addressFrom,
+  amount,
+  addressTo,
+}: TransactionCardProps) => {
+  return (
+    <div
+      className='bg-[#181918] m-4 flex flex-1
+      2xl:min-w-[450px]
+      2xl:max-w-[500px]
+      sm:min-w-[270px]
+      sm:max-w-[3000px]
+      flex-col p-3 rounded-md hover:shadow-2xl
+    '
+    >
+      <div className='flex flex-col items-center w-full mt-3'>
+        <div className='w-full mb-6 p-2'>
+          <a
+            href={`https://rinkeby.etherscan.io/address/${addressFrom}`}
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <p className='text-white text-base'>
+              From: {shortenAddress(addressFrom)}
+            </p>
+          </a>
+          <a
+            href={`https://rinkeby.etherscan.io/address/${addressTo}`}
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <p className='text-white text-base'>
+              To: {shortenAddress(addressTo)}
+            </p>
+          </a>
+          <p className='text-white text-base'>Amount: {amount}</p>
+          {message && (
+            <>
+              <br />
+              <p className='text-white text-base'>Message: {message}</p>
+            </>
+          )}
+
+          <div className='bg-black p-3 px-5 w-max rounded-3xl -mt-5 shadow-2xl'>
+            <p className='text-[#37c7da] font-bold'>{timestamp}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
 
 export const Transactions = () => {
   const { currentAccount } = useTransactions()
@@ -16,6 +83,12 @@ export const Transactions = () => {
             Connect your account to see the latest transactions
           </h3>
         )}
+
+        <div className='flex flex-wrap justify-center items-center mt-10'>
+          {dummyData.reverse().map((transaction, index) => (
+            <TransactionCard key={transaction.id} {...transaction} />
+          ))}
+        </div>
       </div>
     </div>
   )
