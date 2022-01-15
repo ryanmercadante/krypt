7f67eb35e75513ec2347aa00fbf9b8d288383450
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2022-01-15 14:17:46 -0500

    delete client folder

--- client/src/components/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './Footer'
-export * from './Loader'
-export * from './Navbar'
-export * from './Services'
-export * from './Transactions'
-export * from './Welcome'
--- client/src/utils/helpers.ts
+++
@@ -1,3 +0,0 @@
-export function shortenAddress(address: string) {
-  return `${address.slice(0, 5)}...${address.slice(address.length - 4)}`
-}
